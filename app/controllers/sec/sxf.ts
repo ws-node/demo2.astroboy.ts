3d6e7e83184461d6836369b0ffbe09dcc8071b95
Big Mogician <sunaoran@youzan.com>
2018-12-15 15:33:09 +0800

    update demo

--- app/controllers/sec/sxf.ts
+++ app/controllers/sec/sxf.ts
@@ -1,16 +1,65 @@
-import { Controller, GET, JsonResult } from "astroboy.ts";
+import { Controller, GET, JsonResult, Context, RenderResult } from "astroboy.ts";
 
-class XXXXXX {
+class WWWWWW {
+
+  constructor(private ctx: any) {
+
+  }
+
+  protected toJSON(value: any): JsonResult {
+    return new JsonResult(value, {
+      jsonTemplate: {
+        status: 200,
+        msg: "success",
+        data: null
+      },
+      jsonTplKey: "data"
+    });
+  }
+
+}
+
+class XXXXXX extends WWWWWW {
+
+  constructor(ctx: any) {
+    super(ctx);
+  }
+
+  protected toJSON(value: any): JsonResult {
+    return new JsonResult(value, {
+      jsonTemplate: {
+        xCode: 0,
+        msg: "success",
+        data: null
+      },
+      jsonTplKey: "data"
+    });
+  }
 
 }
 
 @Controller("bbb")
 class TestController extends XXXXXX {
 
+  constructor(private context: Context) {
+    super(context.ctx);
+  }
+
   @GET("xxcdc")
   public xxx() {
-    return new JsonResult({
+    return this.toJSON({
       abc: 123
+    });
+  }
+
+  @GET("xxcdc2")
+  public xxx2() {
+    return new RenderResult({
+      path: "",
+      engine: "xxx",
+      engines: {
+        xxx: undefined
+      }
     });
   }
 

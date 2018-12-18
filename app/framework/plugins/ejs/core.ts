2372fa4ab7822f751b6387fcec9835ca4a114865
Big Mogician <sunaoran@youzan.com>
2018-12-18 22:44:30 +0800

    update demo rc.10

--- app/framework/plugins/ejs/core.ts
+++ app/framework/plugins/ejs/core.ts
@@ -1,4 +1,5 @@
 import ejs, { Options } from "ejs";
+import path from "path";
 import { Injectable, IViewEngine, Configs, Render } from "astroboy.ts";
 import { EJS_ENGINE_OPTIONS } from "./options";
 
@@ -11,11 +12,19 @@ export class EjsEngine implements IViewEngine {
 
   public async render(name: string, configs: any): Promise<string> {
     const cf = this.configs;
-    return ejs.renderFile(`${cf.root}/${name}`, this.rs.views, <any>{ ...(cf || {}), ...configs, async: true });
+    return ejs.renderFile(
+      path.isAbsolute(name) ? name : `${cf.root}/${name}`,
+      this.rs.views,
+      <any>{ ...(cf || {}), ...configs, async: true }
+    );
   }
 
   public async renderString(tpl: string, configs: any): Promise<string> {
-    return ejs.render(tpl, this.rs.views, <Options & { async: true }>{ ...this.configs, ...configs, async: true });
+    return ejs.render(
+      tpl,
+      this.rs.views,
+      <Options & { async: true }>{ ...this.configs, ...configs, async: true }
+    );
   }
 
 }

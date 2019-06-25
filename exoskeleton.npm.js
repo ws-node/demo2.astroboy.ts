87221fbb4c93d2fedc9f4bd4f508cdaf1131de16
Big Mogician <sunaoran@youzan.com>
2019-06-25 17:36:37 +0800

    fix: publish new v

--- exoskeleton.npm.js
+++ exoskeleton.npm.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const config = require("./atc.config");
+const config = require("./exoskeleton.config");
 const { merge } = require("@exoskeleton/cli/cmd");
 
 module.exports = merge(config, {
